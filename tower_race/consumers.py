4865e3153a261ac8ad7a331b790cca28127e4d1b
Brendan <bgmcmullen@comcast.net>
2024-12-03 00:08:21 -0800

    refactored project

--- tower_race/consumers.py
+++ tower_race/consumers.py
@@ -3,7 +3,6 @@ from channels.generic.websocket import WebsocketConsumer
 
 from  tower_race.main import Game
 
-
 class GameConsumer(WebsocketConsumer):
   def connect(self):
     self.game = Game()
@@ -17,12 +16,11 @@ class GameConsumer(WebsocketConsumer):
     print('data', data)
     type = data["type"]
     switch = {
-      "test_message": self.test,
       "start_game": self.start_game,
       'take_from_discard': self.take_from_discard,
-      'take_from_main': self.take_from_main,
+      'take_from_hidden': self.take_from_hidden,
       'get_top_of_discard': self.get_top_of_discard,
-      'get_top_of_main': self.get_top_of_main_pile
+      'get_top_of_hidden': self.get_top_of_hidden_pile
     }
     try:
       handler = switch[type]
@@ -31,116 +29,78 @@ class GameConsumer(WebsocketConsumer):
     except KeyError:
       print('no task')
 
-
   def get_top_of_discard(self, payload):
     brick = self.game.top_of_pile("discard")
-    self.send(text_data=json.dumps({
-      'payload': brick,
-      'type': "set_next_brick",
-    }))
+    self.send_event(brick, "set_next_brick")
 
-  def get_top_of_main_pile(self, payload):
-    brick = self.game.top_of_pile("main")
-    self.send(text_data=json.dumps({
-      'payload': brick,
-      'type': "set_next_brick",
-    }))
+  def get_top_of_hidden_pile(self, payload):
+    brick = self.game.top_of_pile("hidden")
+    self.send_event(brick, "set_next_brick")
 
+  # take a new brick and replace a brick in the tower
+  def take_new_brick(self, brick_to_replace, stack):
 
-  def take_from_discard(self, payload):
-    playertower = self.game.take_from_pile('discard', int(payload))
-    self.send(text_data=json.dumps({
-      'payload': playertower,
-      'type': "set_player_tower",
-    }))
+    # Get updated player tower
+    playertower = self.game.take_from_pile(stack, int(brick_to_replace))
 
+    # Send updated player tower
+    self.send_event(playertower, "set_player_tower")
+
+    # Check if computer has won
     if self.game.check_tower_blaster('player') == True:
-      self.send(text_data=json.dumps({
-        'payload': None,
-        'type': "player_wins",
-      }))
+      self.send_event(None, "player_wins")
       return
 
-
+    # Play computer turn
     computer_turn_status = self.game.play_computers_turn()
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['game_text'],
-      'type': "set_computer_play_message",
-    }))
 
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['computer_tower'],
-      'type': "set_computer_tower",
-    }))
+    # Send computer status
+    self.set_computer_turn_status(computer_turn_status)
 
+    # Check if computer has won
     if self.game.check_tower_blaster('computer') == True:
-      self.send(text_data=json.dumps({
-        'payload': None,
-        'type': "computer_wins",
-      }))
+      self.send_event(None, "computer_wins")
+
+
+  def take_from_discard(self, brick_to_replace):
+    self.take_new_brick(brick_to_replace, 'discard')
   
-  def take_from_main(self, payload):
-    playertower = self.game.take_from_pile('main', int(payload))
-    self.send(text_data=json.dumps({
-      'payload': playertower,
-      'type': "set_player_tower",
-    }))
-
-    if self.game.check_tower_blaster('player') == True:
-      self.send(text_data=json.dumps({
-        'payload': None,
-        'type': "player_wins",
-      }))
-      return
-
-    computer_turn_status = self.game.play_computers_turn()
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['game_text'],
-      'type': "set_computer_play_message",
-    }))
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['computer_tower'],
-      'type': "set_computer_tower",
-    }))
-
-    if self.game.check_tower_blaster('computer') == True:
-      self.send(text_data=json.dumps({
-        'payload': None,
-        'type': "computer_wins",
-      }))
+  def take_from_hidden(self, brick_to_replace):
+    self.take_new_brick(brick_to_replace, 'hidden')
 
 
 
+  # Begin new game
   def start_game(self, incoming_payload):
-    payload = self.game.start()
-    self.send(text_data=json.dumps({
-      'payload': payload['computer_tower'],
-      'type': "set_computer_tower",
-    }))
-    self.send(text_data=json.dumps({
-      'payload': payload['player_tower'],
-      'type': "set_player_tower",
-    }))
+
+    # Get starting tower status
+    tower_status = self.game.start()
+
+    # Send player and computer tower status
+    self.send_event(tower_status['computer_tower'], "set_computer_tower")
+    self.send_event(tower_status['player_tower'], "set_player_tower")
+
+    # Play computer turn
     computer_turn_status = self.game.play_computers_turn()
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['game_text'],
-      'type': "set_computer_play_message",
-    }))
 
-    self.send(text_data=json.dumps({
-      'payload': computer_turn_status['computer_tower'],
-      'type': "set_computer_tower",
-    }))
+    # Send computer status
+    self.set_computer_turn_status(computer_turn_status)
+
+    # Send computer winning message if applicable
     if computer_turn_status['computer_wins'] == True:
-      self.send(text_data=json.dumps({
-        'payload': None,
-        'type': "computer_wins",
-      }))
+      self.send_event(None, "computer_wins")
 
 
-  def test(self, payload):
-    print('connection test')
+  # Send computer tower status and computer play text
+  def set_computer_turn_status(self, computer_turn_status):
+    self.send_event(computer_turn_status['game_text'], "set_computer_play_message")
+
+    self.send_event(computer_turn_status['computer_tower'], "set_computer_tower")
+
+  # Send event to client.
+  def send_event(self, payload, type):
     self.send(text_data=json.dumps({
-      'payload': 'web socket is working',
-      'type': "test",
+      'payload': payload,
+      'type': type,
     }))
+
